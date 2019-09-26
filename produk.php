30822088baf52180fc981fc6a6ce934757bbfb88
zikrial <zikrialhaq@mail.unpas.ac.id>
2019-09-26 17:09:33 +0700

    Menambahkan properti pada class Produk

--- produk.php
+++ produk.php
@@ -1,12 +1,15 @@
 <?php
 
 class Produk {
-
+    public $judul,
+           $penulis,
+           $penerbit,
+           $harga;
 
 }
 
 $produk1 = new Produk();
-$produk2 = new Produk();
+var_dump($produk1);
 
 
 
