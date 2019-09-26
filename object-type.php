111fb65ee9cd2e8b350592354ee36e8924744354
zikrial <zikrialhaq@mail.unpas.ac.id>
2019-09-27 05:29:35 +0700

    Menambahkan file object-type.php

---
+++ object-type.php
@@ -0,0 +1,30 @@
+<?php
+
+class Produk {
+    public $judul,
+           $penulis,
+           $penerbit,
+           $harga = 0;
+
+    public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0) {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+    public function getLabel() {
+        return "$this->penulis, $this->penerbit";
+    }
+
+}
+
+$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump",30000);
+$produk2 = new Produk("Uncharted", "Neil Drukmann", "Sony Computer", 250000);
+
+echo "Komik : " . $produk1->getLabel();
+echo "<br>";
+echo "Game : ". $produk2->getLabel();
+
+
+?>
