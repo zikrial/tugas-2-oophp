0888029778aa41c6b89b6dac5ed8c06dddc711a8
zikrial <zikrialhaq@mail.unpas.ac.id>
2019-09-27 05:40:47 +0700

    Membuat class baru untuk menampilkan isi produk

--- object-type.php
+++ object-type.php
@@ -19,12 +19,23 @@ class Produk {
 
 }
 
+class CetakInfoProduk {
+    public function cetak( Produk $produk ) {
+        $str =  "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+        return $str;
+    }
+}
+
 $produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump",30000);
 $produk2 = new Produk("Uncharted", "Neil Drukmann", "Sony Computer", 250000);
 
 echo "Komik : " . $produk1->getLabel();
 echo "<br>";
 echo "Game : ". $produk2->getLabel();
+echo "<br>";
+
+$InfoProduk1 = new CetakInfoProduk();
+echo $InfoProduk1->cetak($produk1);
 
 
 ?>
