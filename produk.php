ae8888fce529da9cc14934ddf5badcba58644c2b
zikrial <zikrialhaq@mail.unpas.ac.id>
2019-09-26 17:20:53 +0700

    Menambahkan nilai pada property dan membuat method

--- produk.php
+++ produk.php
@@ -1,15 +1,33 @@
 <?php
 
 class Produk {
-    public $judul,
-           $penulis,
-           $penerbit,
-           $harga;
+    public $judul = "judul",
+           $penulis = "penulis",
+           $penerbit = "penerbit",
+           $harga = 0;
+
+    public function sayHello() {
+        return "Hello World!";
+    }
 
 }
 
 $produk1 = new Produk();
+$produk1->judul = "Naruto";
 var_dump($produk1);
+
+$produk2 = new Produk();
+$produk2->judul = "Uncharted";
+$produk2->tambahProperty = "lol";
+var_dump($produk2);
+
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kishimoto";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = 30000;
+
+echo "Komik : $produk3->judul, $produk3->penulis";
 
 
 
