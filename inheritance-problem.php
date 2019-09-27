5757538c3150a89dfa4d5d1111eff62f5557d20d
zikrial <zikrialhaq@mail.unpas.ac.id>
2019-09-27 17:41:37 +0700

    Menambahkan fungsi getInfoLengkap pada inheritance-problem.php

--- inheritance-problem.php
+++ inheritance-problem.php
@@ -4,17 +4,34 @@ class Produk {
     public $judul,
            $penulis,
            $penerbit,
-           $harga = 0;
+           $harga,
+           $jmlHalaman,
+           $waktuMain,
+           $tipe;
 
-    public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0) {
+    public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0, $tipe) {
         $this->judul = $judul;
         $this->penulis = $penulis;
         $this->penerbit = $penerbit;
         $this->harga = $harga;
+        $this->jmlHalaman = $jmlHalaman;
+        $this->waktuMain = $waktuMain;
+        $this->tipe = $tipe;
     }
 
     public function getLabel() {
         return "$this->penulis, $this->penerbit";
+    }
+
+    public function getInfoLengkap() {
+        $str = "{$this->tipe} : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+        if ($this->tipe == "Komik") {
+            $str .=  " - {$this->jmlHalaman} Halaman.";
+        } else if ($this->tipe == "Game") {
+            $str .=  " - {$this->waktuMain} Jam.";
+        }
+        return $str;
+
     }
 
 }
@@ -26,16 +43,11 @@ class CetakInfoProduk {
     }
 }
 
-$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump",30000);
-$produk2 = new Produk("Uncharted", "Neil Drukmann", "Sony Computer", 250000);
+$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump",30000, 100, 0, "Komik");
+$produk2 = new Produk("Uncharted", "Neil Drukmann", "Sony Computer", 250000, 0, 50, "Game");
 
-echo "Komik : " . $produk1->getLabel();
-echo "<br>";
-echo "Game : ". $produk2->getLabel();
-echo "<br>";
-
-$InfoProduk1 = new CetakInfoProduk();
-echo $InfoProduk1->cetak($produk1);
-
+echo $produk1->getInfoLengkap();
+echo"<br>";
+echo $produk2->getInfoLengkap();
 
 ?>
