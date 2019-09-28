2c71e6abb947a178b05c0eae4b9d4a5f0b519317
zikrial <zikrialhaq@mail.unpas.ac.id>
2019-09-29 06:12:26 +0700

    Membuat konstanta

--- constant.php
+++ constant.php
@@ -1,5 +1,29 @@
 <?php
 
+// define('NAMA', 'Zikri');
+// echo NAMA;
 
+// const UMUR = 32;
+// echo UMUR;
+
+// class Coba {
+//     const NAMA = 'Zikri';
+// }
+
+// echo Coba::NAMA;
+
+// echo __FILE__;
+
+// function coba() {
+//     return __FUNCTION__;
+// }
+// echo coba();
+
+class Coba {
+    public $kelas = __CLASS__;
+}
+
+$obj  = new Coba;
+echo $obj->kelas;
 
 ?>
