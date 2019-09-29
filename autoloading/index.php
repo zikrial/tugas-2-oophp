755b601340974fa6e150a9bd902b8acb33bd3353
zikrial <zikrialhaq@mail.unpas.ac.id>
2019-09-29 07:18:22 +0700

    Melakukan autoloading

--- autoloading/index.php
+++ autoloading/index.php
@@ -1,5 +1,8 @@
 <?php
 
+require_once 'App/init.php';
+
+
 $produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump",30000, 100);
 $produk2 = new Game("Uncharted", "Neil Drukmann", "Sony Computer", 250000, 50);
 
